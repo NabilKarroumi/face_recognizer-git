9b4f6e239b3b6c9da36d1fdb4ed186fad3d0f66f
NabilKarroumi <nkarb@hotmail.fr>
2021-03-04 13:02:29 +0100

    Quick fix:
    - Removing useless line from face_recognizer_launcher.bat

--- face_recognizer_launcher.bat
+++ face_recognizer_launcher.bat
@@ -1,7 +1,6 @@
 @echo off
 title The application 'Face Recognizer' is starting ...
 echo Starting ...
-@REM python -m "D:\Users\KARROUMI Nabil\Desktop\ApprendrePython\PROJECTS\FacesRecognition\face_recognizer"
 python -m face_recognizer
 echo Thank you, the application will close.
 echo See you soon !!
